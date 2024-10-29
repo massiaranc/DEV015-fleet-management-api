526167037064d7368536ce7df6c2baf1617487a4
massiaranc <marancibia4@gmail.com>
2024-10-29 13:15:20 -0300

    agrega conexión con  la database

--- hello.py
+++ hello.py
@@ -1,20 +1,35 @@
 """ 
 primer interacción
 """
-from flask import Flask
+from flask import Flask , jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Base(DeclarativeBase):
+    pass
+#crea la app
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://default:@ep-calm-hat-a4yy392i-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")#CARGA LA URL DESDE .ENV
 
 
-@app.route("/")
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+
+@app.route('/' , methods=['GET'])
 def hello_world():
     """ devuelve un hola en la pag"""
-    return "<p>Hollo, World!</p>"
+    return jsonify(mensaje= "hola Mundo")
 
 @app.route('/taxis')
 def taxis():
     """ creacion de ruta taxis"""
     return 'Taxis'
+
+if __name__ == '__main__':
+    app.run(debug=True)
